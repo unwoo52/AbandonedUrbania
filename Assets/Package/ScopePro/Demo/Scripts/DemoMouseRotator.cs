3153d04f9fa9347ce9b1e1eb30dc0bf5fd5cb86f
Unwoo <73688472+unwoo52@users.noreply.github.com>
2023-04-09 00:36:30 +0900

    scope 줌 기능 추가

--- Assets/Package/ScopePro/Demo/Scripts/DemoMouseRotator.cs
+++ Assets/Package/ScopePro/Demo/Scripts/DemoMouseRotator.cs
@@ -32,8 +32,14 @@ namespace Lovatto.Demo.ScopePro
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
+
+            dotsiteSize = ZoomMaterial.GetFloat("Vector1_0bb2c494708d4e73aed6ec3922b741ac");
+            ZoomSize = ZoomMaterial.GetFloat("_RenderSize");
+
+
         }
 
+        [Header("스코프 움직임")]
         [Tooltip("값이 클수록 마우스를 움직일 때 카메라가 크게 회전합니다.")]
         public float CameraAngle;
         [Tooltip("값이 클수록 마우스를 움직일 때 스코프가 크게 회전합니다.")]
@@ -44,34 +50,56 @@ namespace Lovatto.Demo.ScopePro
         public float MinMax;
         [Tooltip("에임의 감도입니다.")]
         public float SensitivityMouseAim;
+
+
+        [Header("스코프 줌")]
+        public Material ZoomMaterial;
+        public float dotsiteSize;
+        public float ZoomSize;
+        public float zoomdefault = 0.5f;
+        public Vector2 DotsiteMinMax;
+        public Vector2 ZoomAreaMinMax;
+        public Vector2 ZoomMinMax;
+
+
+        [Tooltip("값이 클수록 마우스휠을 굴릴 때 줌이 더 빨리 커지고 줄어듭니다.")]
+        public float ZoomSpeed;
         private void Update()
         {
-            Vector2 output = new Vector2(Input.GetAxis("Camera X"), Input.GetAxis("Camera Y")) * SensitivityMouseAim; //vector2(Input.GetAxis("camera X"),Input.GetAxis("Camera Y"))
+                //add zoom
+            float zoomvalue = Input.GetAxis("Camera Zoom") * ZoomSpeed;
+            zoomdefault -= zoomvalue;
+            zoomdefault = Mathf.Clamp(zoomdefault + zoomvalue, ZoomMinMax.x, ZoomMinMax.y);
+
+
+            dotsiteSize = Mathf.Clamp(dotsiteSize + zoomvalue, -DotsiteMinMax.x, DotsiteMinMax.y);
+            ZoomSize = Mathf.Clamp(ZoomSize + zoomvalue, -ZoomAreaMinMax.x, ZoomAreaMinMax.y);
+
+            float k = Mathf.Lerp(DotsiteMinMax.x, DotsiteMinMax.y, zoomdefault);
+            float m = Mathf.Lerp(ZoomAreaMinMax.x, ZoomAreaMinMax.y, zoomdefault);
+            //add Dot Sight
+            ZoomMaterial.SetFloat("Vector1_0bb2c494708d4e73aed6ec3922b741ac", k);
+            //add Scope Zoom size
+            ZoomMaterial.SetFloat("_RenderSize", m);
+
+
+                //add camPos camAngle scopeAngle
+            Vector2 output = new Vector2(Input.GetAxis("Camera X"), Input.GetAxis("Camera Y")) * SensitivityMouseAim;
             Vector3 euler = transform.eulerAngles;
 
-            /*
-            euler.y = -Mathf.Lerp(HorizontalMinMax.x, HorizontalMinMax.y, output.y);
-            euler.x = Mathf.Lerp(VerticalMinMax.x, VerticalMinMax.y, output.x);
-            euler.y += 180;
-            */
-
-            //testA -= output.y;
-            //testA = Mathf.Clamp(testA + output.y, VerticalMinMax.x, VerticalMinMax.y);
             testA = Mathf.Clamp(testA + output.y, -MinMax, MinMax);
-            //testB += output.x;
-            //testB = Mathf.Clamp(testB + output.x, HorizontalMinMax.x, HorizontalMinMax.y);
             testB = Mathf.Clamp(testB + output.x, -MinMax, MinMax);
 
             euler.x = -testA;
             euler.y = testB;
 
-            Debug.Log(euler + "::::" + output);
-
+            //add Scope Angle
             RotCenter.transform.eulerAngles = defaultRot + euler * CenterAngle;
+            //add Cam Angle
             m_Camera.transform.eulerAngles = (camOriginRot + euler) * CameraAngle;
 
+            //add Cam Transform
             Vector3 newver = new Vector3(euler.y, -euler.x, 0);
-            Debug.Log(newver);
             RotCenter.transform.position = (CenterOriginPos + newver * CenterPos);
         }
     }
